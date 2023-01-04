5221889c6958f4e5f004a3f13182897d28f4818f
denilsondev <denilsoncdc10@gmail.com>
2023-01-03 21:15:26 -0300

    first commit

---
+++ ProjetoAulas/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("HPrimeiro programa");
+
+
+int idade = 33;
+System.Console.WriteLine(idade);
+
+string nomePessoa = "Denilson";
+System.Console.WriteLine(nomePessoa);
+
+decimal valor = 200.39m;
+System.Console.WriteLine(valor);
